1fa680bc1847823a9a5e2324fbbce74435c88545
pablosaravia@gmail.com <pablosaravia@gmail.com>
2019-07-31 01:59:08 -0300

    segundo commit

--- practicos/prac1/punto.cpp
+++ practicos/prac1/punto.cpp
@@ -2,6 +2,13 @@
 
 punto::punto()
 {
+    this->abs=0;
+    this->ord=0;
+}
+
+punto::punto(float *x, float *y){
+    this->abs = *x;
+    this->ord = *y;
 }
 
 punto::~punto()
--- practicos/prac1/segmento.h
+++ practicos/prac1/segmento.h
@@ -1,10 +1,22 @@
 #ifndef SEGMENTO_H
 #define SEGMENTO_H
 
+#include "punto.h"
+
 class segmento
 {
+private:
+    punto *orig;
+    punto *dest;
+    
 public:
     segmento();
+    segmento (punto *a, punto *b);
+    punto get_orig();
+    punto get_dest();
+    void set_orig(punto *a);
+    void set_dest(punto *b);
+    float get_largo ();
     ~segmento();
 
 };
