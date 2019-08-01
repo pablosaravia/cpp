6ab4f8b36d229d596189f646889dfd6f8e347168
pablosaravia@gmail.com <pablosaravia@gmail.com>
2019-08-01 08:22:08 -0300

    segmento.cpp

--- practicos/prac1/segmento.cpp
+++ practicos/prac1/segmento.cpp
@@ -7,11 +7,30 @@ segmento::segmento()
 }
 
 segmento::segmento(punto *a, punto *b){
-	this->a = new punto((*a).get_x(),a->get_y());
+	this->a = new punto(a->get_x(),a->get_y());
 	this->b = new punto(b->get_x(),b->get_y());
 }
 
-segmento::~segmento()
-{
+punto segmento::get_a(){
+	punto *Pout = new punto(this->a->get_x(),this->a->get_y());
+	return *Pout;
+}
+
+punto segmento::get_b(){
+	punto *Pout = new punto(this->b->get_x(),this->b->get_y());
+	return *Pout;
+}
+
+void segmento::set_a(punto *a){
+	this->a = new punto(a->get_x(), a->get_y());
+}
+
+void segmento::set_b(punto *b){
+	this->b = new punto(b->get_x(),b->get_y());
+}
+
+segmento::~segmento(){
+	delete this->a;
+	delete this->b;
 }
 
